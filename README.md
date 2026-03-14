99c5a2cac7d069ff27dbd48feff22b1767266d86
Lar97 <dr.number@yandex.ru>
2026-03-14 15:53:32 +0700

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+pip3 install -r requirements.txt
+
+ D:\Lar97\source\repos\SIBSTRIN\semestr_10\dev_software_app\Lab1__PyQT_Introduction_Week_1> D:\Lar97\source\repos\SIBSTRIN\semestr_10\dev_software_app\Lab1__PyQT_Introduction_Week_1\venv\Scripts\python.exe d:/Lar97/source/repos/SIBSTRIN/semestr_10/dev_software_app/Lab1__PyQT_Introduction_Week_1/main.py
---
+++ main.py
@@ -0,0 +1,21 @@
+import sys 
+from PyQt6.QtWidgets import QApplication, QWidget 
+
+class EmptyWindow(QWidget): 
+    def __init__(self):
+        """Конструктор для класса "Пустое окно"""
+        super().__init__()
+        self.initializeUI() 
+        
+    def initializeUI(self): 
+        """Настройка приложения."""
+        self.setGeometry(200, 100, 400, 300)
+        self.setWindowTitle("Моё первое пустое окно в PyQt")
+        self.show() # Отображение окна на экране 
+        
+# Запустить программу
+
+if __name__ == '__main__': 
+    app = QApplication(sys.argv) 
+    window = EmptyWindow() 
+    sys.exit(app.exec())
